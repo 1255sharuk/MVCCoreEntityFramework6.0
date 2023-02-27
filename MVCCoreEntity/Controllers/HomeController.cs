f72c667b68faf7bcbd30fb31b0cfbff0a51b49bd
1255SHARUK <86429569+1255sharuk@users.noreply.github.com>
2023-02-27 22:42:34 +0530

    Add project files.

---
+++ MVCCoreEntity/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVCCoreEntity.Models;
+using System.Diagnostics;
+
+
+namespace MVCCoreEntity.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly NorthwindContext NorthwindContext;
+        public HomeController(ILogger<HomeController> logger, NorthwindContext NorthwindContext)
+        {
+            _logger = logger;
+            this.NorthwindContext = NorthwindContext;
+        }
+
+        public IActionResult Index()
+        {
+            var customers = NorthwindContext.Customers.ToList();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
